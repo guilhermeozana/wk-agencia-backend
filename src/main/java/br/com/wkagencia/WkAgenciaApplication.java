d82e25c13c702d90b931ba2b60254e93fca1dc66
guilhermeozana <guilhermeozana@hotmail.com>
2021-05-17 19:44:58 -0300

    primeiro commit

---
+++ src/main/java/br/com/wkagencia/WkAgenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.wkagencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkAgenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkAgenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wkagencia/WkAgenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wkagencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WkAgenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
